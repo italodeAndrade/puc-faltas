349a4aff1e1cbe90ddcc35521ea0d3d165132b16
Ítalo de Andrade <italolande@gmail.com>
2024-09-27 21:30:20 -0300

    sqlite

--- faltas puc/software.py
+++ faltas puc/software.py
@@ -2,49 +2,32 @@ import subprocess
 import time
 import os
 import getpass
-
-class matéria:
-    def __init__ (self,aluno ,crg_horaria, qnt_falta) :
-        self.crg_horaria= crg_horaria
-        self.qnt_falta= qnt_falta
-        self.nome=aluno
-    
-    def print_cargah(self):
-        carga= self.crg_horaria * (100 / 25)
-        falta=self.qnt_falta
-        if falta == carga:
-            print(" você esta reprovado por falta")
-    
-    def print_faltas(self):
-        carga= self.crg_horaria * (100 / 25)
-        pd_faltar= carga - self.qnt_falta
-        if pd_faltar != self.qnt_falta: 
-            print(f"você ja faltou {self.qnt_falta} porém vc pode faltar {pd_faltar}")
-        else:
-            print(f"você ja reprovou por falta porra, você faltou {self.qnt_falta} você poderia faltar somente {pd_faltar}")
-
+import sqlite3
 
 def cadastrar_user():
     usuario=input("insira o seu usuario")
     senha=input("insira a sua senha")
-    with open ('files/usuarios.txt', 'w') as arquivo_cadastro:
-        linha=f"{usuario}, {senha} "
-        arquivo_cadastro.write(linha)
-def logar_user(nome_usuario, senha):
- with open('files/usuarios.txt', 'r') as login_arquivo:
-    linhas = login_arquivo.readlines()
-    for linha in linhas:
-        partes = linha.strip().split(',')
-        usuario = None
-        senha_usuario = None
-        for parte in partes:
-            if 'usuario' in parte:
-                usuario = parte.split(',')[1].strip()
-            elif 'senha' in parte:
-                senha_usuario = parte.split(',')[1].strip()
-        if usuario == nome_usuario and senha_usuario == senha:
-         return True
-    return False     
+    cursor.execute(" INSERT INTO usuarios values (? , ?)" , (usuario, senha))
+    
+
+def logar_user(id_user):
+    login_vld= False
+    usuario= str(input("insira o seu nome de usuario: "))
+    senha = str(input("insira a sua senha: "))
+    
+    try:
+        cursor.execute("SELECT * FROM usuarios WHERE nome = ? && senha= ?"(usuario, senha))
+        if cursor.fetchone():
+            cursor.execute("SELECT id FROM usuarios WHERE nome = ?"(usuario,))
+            id_user=cursor.fetchone()
+            login_vld= True
+            print(f"!! bem vindo {usuario} !!")
+        
+    except Exception as e:
+        print(f"ocorreu um erro : {e}")
+    return login_vld
+        
+    
 def criar_materia(nome):
     carga_horaria=input("insira a carga horaria da matéria: ")
     quantidade_falta= input("insira a sua quantidade de faltas: ")
@@ -52,5 +35,21 @@ def criar_materia(nome):
     with open('files/materias.txt', 'w') as matérias_arquivo:
         linha= f"{aluno} , {carga_horaria} , {quantidade_falta}"
         matérias_arquivo.write(linha)
+  
+
+
+  
+
 while true:
-    ...
+    conn = sqlite3.connect('usuarios.db')            
+    cursor= conn.cursor
+    cursor.execute("PRAGMA foreign_keys = ON;")
+    cursor.execute( "CREATE TABLE usuarios (id INTERGER PRIMARY KEY AUTOINCREMENT , nome text not null, senha varchar not null)")
+    cursor.execute("CREATE TABLE materias (id INTERGER PRIMARY KEY AUTOINCREMENT, nome_mtr TEXT NOT NULL, FOREING KEY (id_usuario) REFERENCE usuarios(id)")
+    
+    
+    
+    
+    
+    conn.commit()
+    conn.close()
